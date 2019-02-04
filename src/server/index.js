4021fe6f383ea012827ae3db27e75efa0f766697
Miguel Angel Angulo Rivera <miguel.angel.angulo72@gmail.com>
2019-02-04 15:54:37 -0400

    Movies example routes and db

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,15 @@
-console.log('It works!');
+const Koa = require('koa');
+const indexRoutes = require('./routes/index');
+const movieRoutes = require('./routes/movies');
+
+const app = new Koa();
+const PORT = process.env.PORT || 1337;
+
+app.use(indexRoutes.routes());
+app.use(movieRoutes.routes());
+
+const server = app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
+
+module.exports = server;
--- test/sample.test.js
+++ test/sample.test.js
@@ -5,7 +5,7 @@ const should = chai.should();
 
 describe('Sample Test', () => {
   it('should pass', (done) => {
-    const sum = 1 + 1;
+    const sum = 2 + 1;
     sum.should.eql(3);
     sum.should.not.eql(4);
     done();
